ad88b8cb829e7372cf518bcfe00c0338b21f823e
Robert Vo <robertvo79@gmail.com>
2015-04-25 02:48:55 -0500

    Added a reference to ICSharpCode.SharpZipLib.dll through Project > Add Reference > Browse. Added a few lines to create the UnZip feature.

--- TEAM1OIE2S/Controllers/DataController.cs
+++ TEAM1OIE2S/Controllers/DataController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.IO;
+using ICSharpCode.SharpZipLib.Zip;
 
 namespace SEProj.Controllers
 {
@@ -35,7 +36,25 @@ namespace SEProj.Controllers
             }
             return RedirectToAction("UploadAndStoreEVARMetaData");
         }
-     
+        
+        public static void UnZip(string SrcFile, string DstFile, int BufferSize)
+        {
+        FileStream fileStreamIn = new FileStream(SrcFile, FileMode.Open, FileAccess.Read);
+        ZipInputStream zipInStream = new ZipInputStream(fileStreamIn);
+        ZipEntry entry = zipInStream.GetNextEntry();
+        FileStream fileStreamOut = new FileStream
+		(DstFile + @"\" + entry.Name, FileMode.Create, FileAccess.Write);
+        int size;
+        byte[] buffer = new byte<BufferSize/>;
+        do
+        {
+            size = zipInStream.Read(buffer, 0, buffer.Length);
+            fileStreamOut.Write(buffer, 0, size);
+        } while (size > 0);
+        zipInStream.Close();
+        fileStreamOut.Close();
+        fileStreamIn.Close();
+        }
 
     }
 }
