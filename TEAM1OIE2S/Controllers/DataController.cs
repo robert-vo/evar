02e170499ec272a1557ef3521efc3f5ce8cb5d8b
Robert Vo <robertvo79@gmail.com>
2015-04-25 23:53:47 -0500

    web.config should be updated to accommodate for a larger file size uploaded datacontroller is updated to reference evil-dicom and I wrote some simple calls to it.

--- TEAM1OIE2S/Controllers/DataController.cs
+++ TEAM1OIE2S/Controllers/DataController.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.IO;
+using EvilDICOM.Core;
+using EvilDICOM.Core.Helpers;
+using TEAM1OIE2S.Models;
 
 namespace SEProj.Controllers
 {
@@ -31,7 +34,11 @@ namespace SEProj.Controllers
                 var fileName = Path.GetFileName(file.FileName);
                 var path = Path.Combine(Server.MapPath("~/App_Data/"), fileName);
                 file.SaveAs(path);
-
+                var dcm = DICOMObject.Read(@"C:\Users\dropbox\Desktop\export\DICOM\I0");
+                var allDescendants = dcm.AllElements;
+                System.Diagnostics.Debug.WriteLine(dcm.FindFirst("00100010")); //Patient's name NONE^NONE
+                System.Diagnostics.Debug.WriteLine(dcm.FindFirst("00080020")); 
+                System.Diagnostics.Debug.WriteLine(dcm.FindFirst("00100030"));
             }
             return RedirectToAction("UploadAndStoreEVARMetaData");
         }
