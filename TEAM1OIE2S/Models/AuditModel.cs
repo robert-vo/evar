75a76e5d37ebcf9cd46f352e7c3fe3ef637b1a81
Robert Vo <robertvo79@gmail.com>
2015-04-27 20:38:44 -0500

    added audit model

---
+++ TEAM1OIE2S/Models/AuditModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.ComponentModel;
+
+namespace TEAM1OIE2S.Models
+{
+    public class AuditModel : Controller
+    {
+        //
+        // GET: /AuditModel/
+
+        [DisplayName("Audits")]
+        public string Audits { get; set; }
+
+        public string Type { get; set; }
+        public string TableName { get; set; }
+        public string FieldName { get; set; }
+        public string OldValue { get; set; }
+        public string NewValue { get; set; }
+        public DateTime UpdateDate { get; set; }
+
+
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
